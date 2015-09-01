01c5cffd284c8638fd446f03dd2da25255eb7ad5
Mu-An Chiou <me@muanchiou.com>
2015-09-01 10:50:10 +0800

    I 💉ly love tests yes pleas e

---
+++ test/test.js
@@ -0,0 +1,54 @@
+var romanize = require('../index.js')
+var test = require('tape')
+var systems = ['WG', 'MPS-II', 'HANYU']
+
+test('2-char name', function (t) {
+  var results = ['Hsuan Chang', 'Shiuan Jang', 'Syuan Jhang']
+
+  systems.forEach(function (sys, i) {
+    var name = romanize('張懸', sys)
+    t.equal(results[i], name, 'translated in ' + sys + ' correctly.')
+  })
+
+  t.end()
+})
+
+test('3-char name', function (t) {
+  var results = ['Mu-An Chiu', 'Mu-An Chiou', 'Mu-An Ciou']
+
+  systems.forEach(function (sys, i) {
+    var name = romanize('秋木安', sys)
+    t.equal(results[i], name, 'translated in ' + sys + ' correctly.')
+  })
+
+  t.end()
+})
+
+test('4-char name', function (t) {
+  var results = ['Chun-Hung Fan Chiang', 'Jiun-Hung Fan Jiang', 'Jyun-Hong Fan Jiang']
+
+  systems.forEach(function (sys, i) {
+    var name = romanize('范姜峻宏', sys)
+    t.equal(results[i], name, 'translated in ' + sys + ' correctly.')
+  })
+
+  t.end()
+})
+
+test('edge cases', function (t) {
+  var nameWithSpaces = romanize('姓  空白')
+  t.equal(nameWithSpaces, 'Kung-Pai Hsing', 'name translated corrently disregarding spaces')
+
+  var heteronymSurname = romanize('解魯')
+  t.equal(heteronymSurname, 'Lu Hsieh', 'heteronym surname has the correct pronunciation')
+
+  t.end()
+})
+
+test('sad paths', function (t) {
+  t.throws(function () {
+    romanize('cool english')
+  }, /Input contains non-Chinese characters/, 'non-Chinese input triggers an exception')
+
+  t.end()
+})
